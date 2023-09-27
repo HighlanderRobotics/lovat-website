fb42bc1f83c24fe494569a984b31905497d78411
MangoSwirl <collin.camerono@gmail.com>
2023-09-26 22:57:43 -0700

    Customize error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="full-center">
+    <div class="content">
+        <div class="sad-face">:(</div>
+        <h1>An error has occurred.</h1>
+        {#if $page.error != null}
+            <p>{$page.error.message}</p>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .full-center {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sad-face {
+        font-size: 40px;
+    }
+
+    p {
+        color: var(--body);
+    }
+</style>
