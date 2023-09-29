c172d5db146b6e77a5915fc062920cf91abb345b
MangoSwirl <collin.camerono@gmail.com>
2023-09-28 22:32:39 -0700

    Optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { imagetools } from 'vite-imagetools';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		imagetools(),
+		sveltekit(),
+	]
 });
