06fe47b083d41f7ce558ce42d0f688b8f7b000af
MangoSwirl <collin.camerono@gmail.com>
2023-11-07 23:30:22 -0800

    Add Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import Navbar from '$lib/Navbar.svelte';
+	import Analytics from '$lib/components/Analytics.svelte';
     import { MagnoliaUIRoot } from 'magnolia-ui-svelte';
 </script>
+
+<Analytics />
 
 <div class="color-scheme-dark">
     <MagnoliaUIRoot>
