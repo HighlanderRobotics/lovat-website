e308f7a5f5ed6a20ce0a6cb3d1142db6734eece5
MangoSwirl <collin.camerono@gmail.com>
2023-11-13 18:18:21 -0800

    enable edge

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			split: false,
+			edge: true,
 		}),
 		csrf: {
 			// Custom logic that checks the origin is implemented in src/hooks.server.ts
