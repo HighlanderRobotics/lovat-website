ea1856e9ab476d2f8dc6f3fe254ef270f6b721f1
MangoSwirl <68089814+MangoSwirl@users.noreply.github.com>
2024-03-08 19:23:15 +0000

    Start pit display

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Navbar from '$lib/Navbar.svelte';
 	import Analytics from '$lib/components/Analytics.svelte';
 	import { MagnoliaUIRoot } from 'magnolia-ui-svelte';
 </script>
@@ -8,9 +7,6 @@
 
 <div class="color-scheme-dark">
 	<MagnoliaUIRoot>
-		<Navbar />
-		<div class="main-content">
-			<slot />
-		</div>
+		<slot />
 	</MagnoliaUIRoot>
 </div>
---
+++ src/routes/pit/display/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+</script>
+
+<svelte:head>
+    <title>Pit Display | Lovat</title>
+</svelte:head>
+
+<div class="display">
+    <div class="content"></div>
+    <div class="footer">
+        
+    </div>
+</div>
