c2cf1e760038254c1645b995777fac179cdacd25
Sophie Bowyer <sophiebowyer@Sophies-MacBook-Air-2.local>
2025-11-06 17:51:33 -0800

    lovat-archive page

---
+++ src/routes/(with-nav)/archive/+page.svelte
@@ -0,0 +1,66 @@
+<svelte:head>
+	<title>Archive | Lovat</title>
+</svelte:head>
+
+<section class="info">
+	<div class="content">
+		<h1>Lovat Archive</h1>
+		<p>Download data from past seasons.</p>
+	</div>
+</section>
+<section class="data">
+	<a href="https://drive.google.com/file/d/1lEbwz9j4SugQhFkUuXNMO9O7FHqqG91f/view?export=download"
+		target="_blank">2024 - Crescendo</a
+	>
+</section>
+
+<style>
+	.info {
+		background-color: var(--secondary-container);
+		padding: 70px 20px;
+	}
+
+	.info .content {
+		max-width: 460px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: var(--on-background);
+		font-size: 44px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: 50px;
+		margin-bottom: 8px;
+		text-align: center;
+	}
+
+	p {
+		color: var(--body);
+		font-size: 18px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+		text-align: center;
+	}
+
+	a {
+		color: var(--victory-purple);
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: normal;
+		text-decoration: none;
+		text-align: center;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	.data {
+		display: flex;
+		justify-content: center;
+		margin-top: 20px;
+	}
+</style>
